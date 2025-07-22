b539a7a698ad6d73b443a496f253d125df436cdd
空指针艺术家 <739153436@qq.com>
2025-07-22 22:19:45 +0800

    feat: doc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+    // // 解决 aria-hidden 警告
+    // experimental: {
+    //     disableDevAccessibilityCheck: true
+    // },
+    //
+    // // 解决 localStorage 问题
+    // devIndicators: {
+    //     buildActivity: false
+    // },
+    // devServer: {
+    //     overlay: false,
+    //     webSocketServer: false
+    // },
+    //
+    // // 可选：禁用 React 严格模式
+    // reactStrictMode: false
 };
 
-export default nextConfig;
+module.exports = nextConfig;
